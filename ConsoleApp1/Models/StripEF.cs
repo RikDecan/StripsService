9bf39a41e1c2bc957b02849b2c43e17bf915a24f
Rik Decan <83696539+RikDecan@users.noreply.github.com>
2025-01-05 15:46:02 +0100

    d

---
+++ ConsoleApp1/Models/StripEF.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    public class StripEF
+    {
+
+        public StripEF()
+        {       
+        }
+
+
+        [Key]
+        public int Id { get; set; }
+        public string Titel { get; set; }
+        public int ReeksNr { get; set; }
+
+
+        //navigational props
+        public List<AutheurEF>  Autheurs{ get; set; }
+        public UitgeverijEF Uitgeverij { get; set; }
+        public ReeksEF Reeks { get; set; }
+
+    }
+
+}
+
---
+++ ConsoleApp1/StripContext.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+
+{
+        //public class StripContext : DbContext
+        //{
+        //    public DbSet<AutheurEF> Autheur { get; set; }
+        //    public DbSet<ReeksEF> Reeks { get; set; }
+        //    public DbSet<StripEF> Strip { get; set; }
+        //    public DbSet<UitgeverijEF> Uitgeverij { get; set; }
+
+
+        //    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //    {
+
+        //    }
+        //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //    {
+        //        optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-8PQJLNFG\SQLEXPRESS;Initial Catalog=Strip;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        //    }
+        //}
+
+
+ 
+}
