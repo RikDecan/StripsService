2db04154684e7375efc38a8acf9a74f9f2331b90
Rik Decan <83696539+RikDecan@users.noreply.github.com>
2025-01-05 16:24:34 +0100

    so far so good

---
+++ StripDL/Program.cs
@@ -0,0 +1,16 @@
+﻿using StripDL;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using (var context = new StripContext())
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            Console.WriteLine("Database is opnieuw aangemaakt");
+
+        }
+    }
+}
